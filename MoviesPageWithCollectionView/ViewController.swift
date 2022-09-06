c755022c77e6d06a15755bba719ec6efa5528db1
ogullcankara <ogullcankara@gmail.com>
2022-09-07 00:56:00 +0300

    Initial Commit

---
+++ MoviesPageWithCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesPageWithCollectionView
+//
+//  Created by Ogulcan Kara on 7.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
